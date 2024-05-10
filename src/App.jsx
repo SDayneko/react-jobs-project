e9832dddbc6474e58e043a8503808f4fdca420d3
Sergey Dayneko <s.daineko@gmail.com>
2024-05-09 21:34:31 -0700

    Add logo and separate page on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import logo from '../src/assets/images/logo.png'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import Navbar from './components/Navbar'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+    </>
   )
 }
 
