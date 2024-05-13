7728ae24ca40064965927d37dfa869df50070262
Sergey Dayneko <s.daineko@gmail.com>
2024-05-13 07:44:49 -0700

    Update links and add Jobs and NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import Navbar from './components/Navbar'
-import ViewAllJobs from './components/ViewAllJobs'
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider 
+} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
-      <Route index element= {<HomePage />} />
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />}/>
+      <Route path='*' element={<NotFoundPage />}/>
     </Route>)
 )
 
