94d2a935ecd9068b40ae6a39a86d6668ea5189f9
Sergey Dayneko <s.daineko@gmail.com>
2024-05-13 21:12:17 -0700

    JSON Server Setup, useEffect() & Data Fetching, Loading Spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,31 @@
-import jobs from '../jobs.json'
+// import jobs from '../jobs.json'
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
+import { FaArchway } from 'react-icons/fa';
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
   // console.log(jobs) // checking if it works
-  const JobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // const JobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      const apiURL = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+      try {
+        const res = await fetch(apiURL);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJob();
+  }, []);
+
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -11,11 +33,13 @@ const JobListings = ({isHome = false}) => {
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         { isHome ? 'Recent Jobs' : 'Browse Jobs'}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        { JobListings.map((job) => (
+        { loading ? (
+          <Spinner loading={loading} />
+        ) : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          { jobs.map((job) => (
           <JobListing key={job.id} job={ job } />
         )) }
-      </div>
+        </div>) }
     </div>
   </section>
   )
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,18 @@
-const Spinner = () => {
-  return (
-    <div>Spinner</div>
-  )
-}
+import ClipLoader from 'react-spinners/ClipLoader';
 
-export default Spinner
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+      color='#4338ca'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+export default Spinner;
